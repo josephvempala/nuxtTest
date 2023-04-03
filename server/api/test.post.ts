b00249f572af8ca0d86934f7ba2bf64d88ce300b
Joseph <joseph@loyalytics.in>
2023-04-03 22:13:47 +0530

    more error handling

--- server/api/test.post.ts
+++ server/api/test.post.ts
@@ -11,46 +11,60 @@ export default defineEventHandler(async (event) => {
             fields: { [item: string]: any };
             files: { [item: string]: { buffer: Buffer; fileInfo: busboy.FileInfo } };
         }>((resolve, reject) => {
-            const fields: { [item: string]: any } = {};
-            const files: {
-                [item: string]: { buffer: Buffer; fileInfo: busboy.FileInfo };
-            } = {};
-            bb.on('file', (name, file, info) => {
-                try {
-                    const _buf: any = [];
-                    file.on('data', (chunk) => _buf.push(chunk));
-                    file.on('end', () => {
-                        files[name] = { buffer: Buffer.concat(_buf), fileInfo: info };
-                        fields[name] = 'file';
-                    });
-                    file.on('close', () => {
-                        files[name] = { buffer: Buffer.concat(_buf), fileInfo: info };
-                        fields[name] = 'file';
-                    });
-                    file.on('error', (err) => reject(err));
-                }
-                catch (e) {
-                    bb.emit('error', e)
-                }
-            });
-            bb.on('field', (name, val, info) => {
-                try {
-                    fields[name] = val;
-                }
-                catch (e) {
-                    bb.emit('error', e)
-                }
-            });
-            bb.on('close', () => {
-                resolve({ fields, files });
-            });
-            bb.on('finish', () => {
-                resolve({ fields, files });
-            });
-            bb.on('error', (e) => {
+            try {
+                const fields: { [item: string]: any } = {};
+                const files: {
+                    [item: string]: { buffer: Buffer; fileInfo: busboy.FileInfo };
+                } = {};
+                bb.on('file', (name, file, info) => {
+                    try {
+                        const _buf: any = [];
+                        file.on('pause', () => {
+                            bb.emit('error', 'PAUSED on file')
+                        })
+                        file.on('data', (chunk) => { try { _buf.push(chunk) } catch (e) { bb.emit(e) } });
+                        file.on('end', () => {
+                            try {
+                                files[name] = { buffer: Buffer.concat(_buf), fileInfo: info };
+                                fields[name] = 'file';
+                            } catch (e) { bb.emit(e) }
+                        });
+                        file.on('close', () => {
+                            try {
+                                files[name] = { buffer: Buffer.concat(_buf), fileInfo: info };
+                                fields[name] = 'file';
+                            } catch (e) { bb.emit(e) }
+                        });
+                        file.on('error', (err) => reject(err));
+                    }
+                    catch (e) {
+                        bb.emit('error', e)
+                    }
+                });
+                bb.on('field', (name, val, info) => {
+                    try {
+                        fields[name] = val;
+                    }
+                    catch (e) {
+                        bb.emit('error', e)
+                    }
+                });
+                bb.on('fieldsLimit', () => bb.emit('error', 'fieldsLimit'))
+                bb.on('filesLimit', () => bb.emit('error', 'filesLimit'))
+                bb.on('partsLimit', () => bb.emit('error', 'partsLimit'))
+                bb.on('close', () => {
+                    resolve({ fields, files });
+                });
+                bb.on('finish', () => {
+                    resolve({ fields, files });
+                });
+                bb.on('error', (e) => {
+                    reject(e);
+                });
+                req.pipe(bb, { end: true })
+            } catch (e) {
                 reject(e);
-            });
-            req.pipe(bb, { end: true })
+            }
         });
         return result;
     }
