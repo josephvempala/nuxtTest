061ddba62571adf2b00c0e1f111b2898948572dd
Joseph <joseph@loyalytics.in>
2023-04-03 21:57:17 +0530

    new changes

--- server/api/test.post.ts
+++ server/api/test.post.ts
@@ -1,45 +1,61 @@
 import busboy from 'busboy';
 export default defineEventHandler(async (event) => {
-    const req = event.node.req;
-    const bb = busboy({
-        headers: req.headers,
-        fileHwm: 1024 * 256,
-        limits: { fileSize: 1024 * 512 },
-    });
-    const result = await new Promise<{
-        fields: { [item: string]: any };
-        files: { [item: string]: { buffer: Buffer; fileInfo: busboy.FileInfo } };
-    }>((resolve, reject) => {
-        const fields: { [item: string]: any } = {};
-        const files: {
-            [item: string]: { buffer: Buffer; fileInfo: busboy.FileInfo };
-        } = {};
-        bb.on('file', (name, file, info) => {
-            const _buf: any = [];
-            file.on('data', (chunk) => _buf.push(chunk));
-            file.on('end', () => {
-                files[name] = { buffer: Buffer.concat(_buf), fileInfo: info };
-                fields[name] = 'file';
+    try {
+        const req = event.node.req;
+        const bb = busboy({
+            headers: req.headers,
+            fileHwm: 1024 * 256,
+            limits: { fileSize: 1024 * 512 },
+        });
+        const result = await new Promise<{
+            fields: { [item: string]: any };
+            files: { [item: string]: { buffer: Buffer; fileInfo: busboy.FileInfo } };
+        }>((resolve, reject) => {
+            const fields: { [item: string]: any } = {};
+            const files: {
+                [item: string]: { buffer: Buffer; fileInfo: busboy.FileInfo };
+            } = {};
+            bb.on('file', (name, file, info) => {
+                try {
+                    const _buf: any = [];
+                    file.on('data', (chunk) => _buf.push(chunk));
+                    file.on('end', () => {
+                        files[name] = { buffer: Buffer.concat(_buf), fileInfo: info };
+                        fields[name] = 'file';
+                    });
+                    file.on('close', () => {
+                        files[name] = { buffer: Buffer.concat(_buf), fileInfo: info };
+                        fields[name] = 'file';
+                    });
+                    file.on('error', (err) => reject(err));
+                }
+                catch (e) {
+                    bb.emit('error', e)
+                }
             });
-            file.on('close', () => {
-                files[name] = { buffer: Buffer.concat(_buf), fileInfo: info };
-                fields[name] = 'file';
+            bb.on('field', (name, val, info) => {
+                try {
+                    fields[name] = val;
+                }
+                catch (e) {
+                    bb.emit('error', e)
+                }
             });
-            file.on('error', (err) => reject(err));
+            bb.on('close', () => {
+                resolve({ fields, files });
+            });
+            bb.on('finish', () => {
+                resolve({ fields, files });
+            });
+            bb.on('error', (e) => {
+                reject(e);
+            });
+            req.pipe(bb, { end: true })
         });
-        bb.on('field', (name, val, info) => {
-            fields[name] = val;
-        });
-        bb.on('close', () => {
-            resolve({ fields, files });
-        });
-        bb.on('finish', () => {
-            resolve({ fields, files });
-        });
-        bb.on('error', (e) => {
-            reject(e);
-        });
-        req.pipe(bb);
-    });
-    return result;
+        return result;
+    }
+    catch (e) {
+        return e;
+    }
+
 })
